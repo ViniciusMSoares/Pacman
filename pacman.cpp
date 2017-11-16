3e8fc767547001b712b1e80cdf0747768ffd9cb7
joaoalcimar <joao.alcimar.junior@gmail.com>
2017-11-15 22:58:05 -0200

    Adição de mais um monstro

--- pacman.cpp
+++ pacman.cpp
@@ -32,10 +32,13 @@ int pacmanX = 14;
 int pacmanY = 10;
 int monstro1X = 2;
 int monstro1Y = 5;
+int monstro2X = 18;
+int monstro2Y = 15;
 bool espacoComPontoMonstro1 = true;
-int listaRecords[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-string listaPlayers[10] = {"------", "------","------", "------", "------",
- "------", "------", "------", "------", "------"};
+bool espacoComPontoMonstro2 = true;
+int listaRecords[10] = {900, 20000, 4000, 232, 500, 100, 50, 30, 10, 150};
+string listaPlayers[10] = {"Joshua", "Crunchy", "NEPTR", "Bonnibel", "Phoebe",
+ "Stormo", "Hanson", "Minerva", "Prismo", "Lumpy"};
 
 void imprimeMapa(){
 	for (int i = 0; i < 20; i++){
@@ -75,8 +78,10 @@ void modificaMapa(int oldX, int oldY){
 }
 
 void iniciaMapa(){
+	// ainda n parei pra testar se um monstro consegue ficar em cima do outro
 	mapa[pacmanX][pacmanY] = 'p';
 	mapa[monstro1X][monstro1Y] = 'm';
+	mapa[monstro2X][monstro2Y] = 'm';
 }
 
 void resetaPosicoes(){
@@ -87,10 +92,16 @@ void resetaPosicoes(){
 	}else{
 		mapa[monstro1X][monstro1Y] = ' ';
 	}
-	mapa[pacmanX][pacmanY] = 'p';
+	if(espacoComPontoMonstro1 == 1){
+		mapa[monstro2X][monstro2Y] = '.';
+	}else{
+		mapa[monstro2X][monstro2Y] = ' ';
+	}
 	
 	monstro1X = 2;
 	monstro1Y = 5;
+	monstro2X = 18;
+	monstro2Y = 15;
 	pacmanX = 14;
 	pacmanY = 10;
 }
@@ -104,7 +115,8 @@ void imprimePontuacao(){
 }
 
 void estaMorto(){
-	if (monstro1X == pacmanX && monstro1Y == pacmanY){
+	if ((monstro1X == pacmanX && monstro1Y == pacmanY) || 
+	(monstro2X == pacmanX && monstro2Y == pacmanY)){
 		numVidas -= 1;
 		resetaPosicoes();
 		iniciaMapa();
@@ -150,11 +162,13 @@ bool mapaVazio(){
 void moveMonsters(){
 	int oldMonster1X = monstro1X;
 	int oldMonster1Y = monstro1Y;
+	int oldMonster2X = monstro2X;
+	int oldMonster2Y = monstro2Y;
 	
-	int chancePerseguirVertical = rand() % 100;
-	
+	int chancePerseguirVerticalM1 = rand() % 100;
+	int chancePerseguirVerticalM2 = rand() % 100;
 
-	if(chancePerseguirVertical > 50){
+	if(chancePerseguirVerticalM1 > 50){
 		if(oldMonster1X < pacmanX){
 			monstro1X += 1;
 		}else{
@@ -168,11 +182,30 @@ void moveMonsters(){
 		}
 	}
 	
-
+	if(chancePerseguirVerticalM2 > 50){
+		if(oldMonster2X < pacmanX){
+			monstro2X += 1;
+		}else{
+			monstro2X -= 1;
+		}
+	}else{
+		if(oldMonster2Y < pacmanY){
+			monstro2Y += 1;
+		}else{
+			monstro2Y -= 1;
+		}
+	}
+	
 	if(ehPonto(monstro1X,monstro1Y)){
 		espacoComPontoMonstro1 = true;	
 	}else{
 		espacoComPontoMonstro1 = false;
+	}
+	
+	if(ehPonto(monstro2X,monstro2Y)){
+		espacoComPontoMonstro2 = true;	
+	}else{
+		espacoComPontoMonstro2 = false;
 	}
 	
 	if(espacoComPontoMonstro1 == 1){
@@ -181,12 +214,23 @@ void moveMonsters(){
 		mapa[oldMonster1X][oldMonster1Y] = ' ';
 	}
 	
+	if(espacoComPontoMonstro2 == 1){
+		mapa[oldMonster2X][oldMonster2Y] = '.';
+	}else{
+		mapa[oldMonster2X][oldMonster2Y] = ' ';
+	}	
+	
 	if (ehParede(monstro1X,monstro1Y)) {
 		monstro1X = oldMonster1X;
 		monstro1Y = oldMonster1Y;
 	}
+	if (ehParede(monstro2X,monstro2Y)) {
+		monstro2X = oldMonster2X;
+		monstro2Y = oldMonster2Y;
+	}
 	
 	mapa[monstro1X][monstro1Y] = 'm';
+	mapa[monstro2X][monstro2Y] = 'm';
 	
 	
 }
@@ -224,16 +268,58 @@ bool move(char dir){
 }
 
 void registraRecord(string nome){
-	listaPlayers[10] = nome;
-	listaRecords[10] = pontuacao;
+	cout << "Sentinela 2 funfando" << endl;
+	listaPlayers[9] = nome;
+	listaRecords[9] = pontuacao;
 	pontuacao = 0;
+	cout << "Sentinela 3 funfando" << endl;
+}
+
+void swapInt(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void swapString(string *xp, string *yp)
+{
+    string temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void sort()
+{
+	int n = 10;
+    int i, j, menorElem;
+ 
+    for (i = 0; i < n-1; i++)
+    {
+        menorElem = i;
+        for (j = i+1; j > n; j++)
+          if (listaRecords[j] > listaRecords[menorElem])
+            menorElem = j;
+ 
+        swapInt(&listaRecords[menorElem], &listaRecords[i]);
+        swapString(&listaPlayers[menorElem], &listaPlayers[i]);
+    }
+}
+
+void listarRecordes(){
+	sort();
+	 
+	cout << "~~~~Hall da Fama~~~~" << endl;
+	for (int i = 0; i < 10 ;i++){
+		cout << "* Nome " << listaPlayers[i] << "\t Pontuacao " << listaRecords[i] << '\n';
+	}
 }
 
 bool isGameOver(){
 	if (numVidas == 0){
 		cout << "Suas vidas acabaram. GAME OVER" << endl;
 		if (pontuacao > listaRecords[10]){
-			cout << "Sua pontuação bateu recorde!!! Deseja entrar no Hall da Fama?" << endl;
+			cout << "Sua pontuacao bateu recorde!!! Deseja entrar no Hall da Fama?" << endl;
 			cout << "1 para sim, 2 para nao" << endl;
 			char opcao;
 			cin >> opcao;
@@ -242,7 +328,9 @@ bool isGameOver(){
 				cout << "Digite seu nome!" << endl;
 				string nome;
 				cin >> nome;
+				cout << "Sentinela 1 funfando" << endl;
 				registraRecord(nome);
+				listarRecordes();
 				break;}
 			case '2':
 				cout << "OK! até a próxima jogador." << endl;
@@ -261,46 +349,6 @@ void imprimeIntro(){
 	cout << "1 - Jogar Pacman" << endl;
 	cout << "2 - Ver Recordes" << endl;
 	cout << "3 - Sair" << endl;
-}
-
-void swapInt(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void swapString(string *xp, string *yp)
-{
-    string temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
- 
-void sort(int arrayRecordes[], string arrayNomes[], int n)
-{
-    int i, j, menorElem;
- 
-    for (i = 0; i < n-1; i++)
-    {
-        menorElem = i;
-        for (j = i+1; j > n; j++)
-          if (arrayRecordes[j] > arrayRecordes[menorElem])
-            menorElem = j;
- 
-        swapInt(&arrayRecordes[menorElem], &arrayRecordes[i]);
-        swapString(&arrayNomes[menorElem], &arrayNomes[i]);
-    }
-}
-
-
-void listarRecordes(){
-	sort(listaRecords, listaPlayers, 10);
-	 
-	cout << "~~~~Hall da Fama~~~~" << endl;
-	for (int i = 0; i < 10 ;i++){
-		cout << "* Nome " << listaPlayers[i] << "\t Pontuacao " << listaRecords[i] << '\n';
-	}
 }
 
 void inicializaJogo(){
